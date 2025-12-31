3d53d1b91cebf5a08edabb684695e64d4ea01a3b
Einstein67 <38520169+Einstein67@users.noreply.github.com>
2025-12-31 07:22:46 +0100

    Update install.py

--- install.py
+++ install.py
@@ -24,7 +24,7 @@ class NetatmoInstaller(ExtensionInstaller):
                     'tokens_persistence_file': 'INSERT_PATH_TO_TOKEN_FILE_HERE',
                     'driver': 'user.netatmo',
                     'mode': 'cloud',
-                    'poll_interval': "600" can currently only be changed directly in the driver [./bin/user/netatmo.py] in line 182',                    
+                    'poll_interval': '600',                    
                 }
             },
             files=[('bin/user', ['bin/user/netatmo.py'])]
